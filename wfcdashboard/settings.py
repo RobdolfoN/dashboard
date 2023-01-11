466d2dc57910ce56e6781d4e8b41d3240904e572
TheSimpleDepartment <111022257+TheSimpleDepartment@users.noreply.github.com>
2023-01-11 00:27:30 -0800

    adding settings.py file

--- wfcdashboard/settings.py
+++ wfcdashboard/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'wfcdashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'DhHDfi43HAy7TFsMWC7a',
+        'HOST': 'containers-us-west-107.railway.app',
+        'PORT': '5989',
     }
 }
 
